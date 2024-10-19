fb0ed9b09e25089801058e3d566dff20a3154207
Nikhil <nikhilgupta.post@gmail.com>
2024-10-19 22:58:47 +0530

    server

---
+++ server/index.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import connectDB from './config/mongoose.js';
+import indexRouter from './Routes/index.js'; 
+import bodyParser from 'body-parser';
+import cors from 'cors';
+const app=express();
+
+app.use(bodyParser.json());
+app.use(cors());
+connectDB();
+app.use('/',indexRouter);
+app.listen(4000,()=>{
+    console.log("server is running on port 4000");
+});
